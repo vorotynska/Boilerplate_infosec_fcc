90bd5d000eb5e465f62a9435cab8f5c134282df8
Irina Carpincic <iryna.vorotynska@gmail.com>
2023-11-30 09:14:05 +0100

    commit

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Information Security with HelmetJS
 
 This is the boilerplate for the Information Security lessons. Instructions for completing these lessons start at https://www.freecodecamp.org/learn/information-security/information-security-with-helmetjs/
+
+
+Replit:  https://replit.com/@vorotynska/boilerplate-infosec#myApp.js
+
+https://boilerplate-infosec--vorotynska.repl.co/
--- myApp.js
+++ myApp.js
@@ -1,6 +1,63 @@
 const express = require('express');
-const app = express();
+const app = express()
+const helmet = require('helmet');
 
+// Hide Potentially Dangerous Information Using helmet.hidePoweredBy()
+app.use(helmet.hidePoweredBy());
+
+// Mitigate the Risk of Clickjacking with helmet.frameguard()
+app.use(helmet.frameguard({
+  action: 'DENY'
+}));
+
+// Mitigate the Risk of Cross Site Scripting (XSS) Attacks with helmet.xssFilter()
+app.use(helmet.xssFilter());
+
+// Avoid Inferring the Response MIME Type with helmet.noSniff()
+app.use(helmet.noSniff());
+
+// Prevent IE from Opening Untrusted HTML with helmet.ieNoOpen()
+app.use(helmet.ieNoOpen());
+
+// Ask Browsers to Access Your Site via HTTPS Only with helmet.hsts()
+app.use(helmet.hsts({
+  maxAge: timeInSeconds = 90 * 24 * 60 * 60,
+  force: true
+}));
+
+// Disable DNS Prefetching with helmet.dnsPrefetchControl()
+app.use(helmet.dnsPrefetchControl());
+
+//Disable Client-Side Caching with helmet.noCache()
+app.use(helmet.noCache());
+
+// Set a Content Security Policy with helmet.contentSecurityPolicy()
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'", 'trusted-cdn.com']
+  }
+}));
+
+// Configure Helmet Using the ‘parent’ helmet() Middleware
+app.use(helmet());
+/*
+app.use(helmet()) will automatically include all the middleware introduced 
+above, except noCache(), and contentSecurityPolicy(), but these can be enabled if necessary. You can also disable or 
+configure any other middleware individually, using a configuration object.
+     app.use(helmet({
+  frameguard: {         // configure
+    action: 'deny'
+  },
+  contentSecurityPolicy: {    // enable and configure
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: ['style.com'],
+    }
+  },
+  dnsPrefetchControl: false     // disable
+}))
+*/
 
 
 
